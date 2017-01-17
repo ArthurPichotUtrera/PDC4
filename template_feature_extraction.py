50ad1a4ffd3b523c935a2600345d96cc08d41d2d
pdumaitre <paul.dumaitre@insa-lyon.fr>
2017-01-17 20:02:17 +0200

    Feature extraction dans une fonction, qui lit un fichier, extrait les features, les inscrit dans un autre fichier.

--- template_feature_extraction.py
+++ template_feature_extraction.py
@@ -1,21 +1,47 @@
 import csv
 import math
 import numpy as np
+#from decision_tree import *
 
-################### Feature Extraction Template #############################
 
 poss_act_0 = ["hotkey"+str(i)+"0" for i in range(10)]
 poss_act_1 = ["hotkey"+str(i)+"1" for i in range(10)]
 poss_act_2 = ["hotkey"+str(i)+"2" for i in range(10)]
-possible_actions = ['s', 'sBase', 'sMineral'] + poss_act_0 + poss_act_1 + poss_act_2
+possible_actions = ['s', 'sBase', 'sMineral'] + poss_act_1 + poss_act_2
 
-filename = "../train.csv"
+################### Feature Extraction #############################
 
-def get_features(filename):
-    # Get data
+def extract_features(input_filename, output_filename, num_rows=9999999999): #Num rows est
+#le nombre max de lignes a traiter. Par defaut pas de limite
+
+    ifile = open(input_filename, 'rb')
+    reader=csv.reader(ifile)
+    ofile = open(output_filename, 'r+')
+
+    rownum = 1
+    newrow = ''
+    for row in reader:
+        #Ici extraction et ecriture des features de chaque ligne
+        if rownum <= num_rows: #Pour verifier sur quelques lignes au debut
+            colnum = 0
+            newrow = row[0] + ", " + row[1] #Le nom du joueur puis la faction
+            for action in possible_actions:
+                newrow += ", "  + str(row.count(action))
+            ofile.write(newrow + "\n") #Inscription de la ligne sur le fichier
+            newrow = ''
+            rownum += 1
+
+    ifile.close()
+    ofile.close()
+    return
+
+####################### Training ###########################
+
+def get_features(features_filename):
+    # Get data from csv into numpy ndarrays
     features = np.ndarray((0,len(possible_actions)))
     classes = np.ndarray((0,1))
-    ifile = open(filename, 'rb')
+    ifile = open(features_filename, 'rb')
     reader = csv.reader(ifile)
     next(reader) # Skip header
 
@@ -34,5 +60,35 @@ def get_features(filename):
         features = np.append(features, [tmp], axis=0)
 
     ifile.close()
-
     return(features, classes)
+
+########################### Predicting #################################
+
+def predict(features_filename, testing_filename, output_filename):
+    lines_train = range(len(features_filename)) #pu un truc dans le genre
+    lines_validate = []
+    model, accuracy = train_validate(features_filename, lines_train, lines_validate)
+    #Todo: ouvrir le fichier testing en read, output en write
+    #Todo: utiliser le modele sur chaque ligne et ecrire le resultat dans l'output.
+    return
+
+####################### Training and Validating #############################
+
+def train_validate(features_filename, lines_train, lines_validate):
+    features, classes = get_features(features_filename)
+    model = [] #train_decision_tree(features_filename, lines_train)
+    accuracy = validate(model, features_filename, lines_validate)
+
+    return model, accuracy
+
+####################### What we actually do ####################################
+
+
+
+input_filename = "first_100_train.csv"
+output_filename = "features_first100_train.csv"
+
+extract_features("first_100_train.csv", "features_first100_train.csv")
+
+
+################################################################################
